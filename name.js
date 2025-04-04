f7a84e84d9c66ba89ee59303322af11c715cf467
Lucas-3435 <nicolasjonb@gmail.com>
2025-04-04 13:31:38 +0800

    Create name.js

---
+++ name.js
@@ -0,0 +1,12 @@
+// Create a new JavaScript file named userProfile.js.
+// Collect the following user information with the use of prompt():
+let fullName = prompt("Enter your full name (first and last name):");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = prompt("Enter your favorite color:");
+
+// Use these variables to store the collected information and log it in the console.
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
